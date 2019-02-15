49ed61ff30d6b0655111b4a05ac630a6117576e3
Juan Pell <jcpell@gmail.com>
2019-02-15 15:48:14 -0300

    Backend-Common-Domain + Public Backend

---
+++ Sales.Common/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Products
+    {
+        [Key]
+        #region Properties
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+        public Decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime PublishOn { get; set; }
+        #endregion
+    }
+}
